e6a18dd418fb3a43e915929fabeaf648ecf0d43b
MarciolimaDev <marclima998@gmail.com>
2026-01-27 11:12:21 -0500

    feat: Add Footer component with social media links, about section, and copyright, including a new image asset.

--- components/main/Footer.tsx
+++ components/main/Footer.tsx
@@ -44,19 +44,17 @@ const Footer = () => {
                     </div>
                     <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
                         <div className="font-bold text-[16px]">Sobre</div>
+
+                        <Link href="/about">
+                            <p className="flex flex-row items-center my-[15px] cursor-pointer">
+                                <span className="text-[15px] ml-[6px]">
+                                    Sobre mim
+                                </span>
+                            </p>
+                        </Link>
                         <p className="flex flex-row items-center my-[15px] cursor-pointer">
                             <span className="text-[15px] ml-[6px]">
-                                Se tornar patrocinador
-                            </span>
-                        </p>
-                        <p className="flex flex-row items-center my-[15px] cursor-pointer">
-                            <span className="text-[15px] ml-[6px]">
-                                Sobre mim
-                            </span>
-                        </p>
-                        <p className="flex flex-row items-center my-[15px] cursor-pointer">
-                            <span className="text-[15px] ml-[6px]">
-                                mifwebchain@gmail.com
+                                contato.cioli@gmail.com
                             </span>
                         </p>
                     </div>
